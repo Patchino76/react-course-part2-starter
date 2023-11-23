49482a2b0689f3cdd660473bcf6cc98babb29b0a
patchino76 <patchino76@gmail.com>
2023-11-23 10:17:44 +0200

    22 - mutaion of Todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import "./App.css";
 import useTodos, { Todo } from "./hooks/useTodos";
 import PostList from "./react-query/PostList";
+import TodoForm from "./react-query/TodoForm";
 import TodoList from "./react-query/TodoList";
 
-// export interface FetchResults {
-//   data: Todo[] | undefined;
-//   error: Error | null;
-//   isLoading: boolean;
-// }
 function App() {
   // const { data, error, isLoading } = useTodos();
   return (
     <>
-      <PostList></PostList>
+      <TodoForm></TodoForm>
+      <TodoList></TodoList>
     </>
   );
 }
--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -1,17 +1,59 @@
-import { useRef } from 'react';
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRef } from "react";
+import { Todo } from "../hooks/useTodos";
+import axios from "axios";
 
 const TodoForm = () => {
   const ref = useRef<HTMLInputElement>(null);
+  const queryClient = useQueryClient();
+  const addTodo = useMutation<Todo, Error, Todo>({
+    mutationFn: (todo: Todo) => {
+      return axios
+        .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
+        .then((res) => res.data);
+    },
+    onSuccess: (savedTodo, newTodo) => {
+      // 1st approach  is to invalidate the query cache, but doesn't work with the jsonplaceholder
+      // queryClient.invalidateQueries({ queryKey: ["todos"] });
+
+      //2 approach: updating the data in the cache directly
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) => {
+        if (!todos) return [savedTodo];
+        return [...todos, savedTodo];
+      });
+    },
+  });
 
   return (
-    <form className="row mb-3">
-      <div className="col">
-        <input ref={ref} type="text" className="form-control" />
-      </div>
-      <div className="col">
-        <button className="btn btn-primary">Add</button>
-      </div>
-    </form>
+    <>
+      {addTodo.error && (
+        <div className="alert alert-danger">{addTodo.error.message}</div>
+      )}
+
+      <form
+        className="row mb-3"
+        onSubmit={(event) => {
+          event.preventDefault();
+          if (ref.current && ref.current.value)
+            addTodo.mutate({
+              id: 0,
+              userId: 1,
+              title: ref.current.value,
+              completed: false,
+            });
+          ref.current!.value = "";
+        }}
+      >
+        <div className="col">
+          <input ref={ref} type="text" className="form-control" />
+        </div>
+        <div className="col">
+          <button className="btn btn-primary">
+            {addTodo.isSuccess ? <p>Add</p> : <p>Adding...</p>}
+          </button>
+        </div>
+      </form>
+    </>
   );
 };
 
